9d8f47681de8b3427738f2278ee97aef2d8745da
Ezra Sandzer-Bell and Tracie Weitzman <tracie@>
2016-12-13 10:09:25 -0800

    add create survey to front end with passing integration spec

--- app.rb
+++ app.rb
@@ -6,3 +6,19 @@ require('./lib/survey')
 require('./lib/question')
 require('pry')
 require('pg')
+
+get('/') do
+  @surveys = Survey.all
+  erb(:index)
+end
+
+get('/new_survey') do
+  erb(:create_survey)
+end
+
+post('/new_survey') do
+  name = params.fetch("name")
+  Survey.create({:name => name})
+  @surveys = Survey.all()
+  erb(:index)
+end
---
+++ spec/integration_spec.rb
@@ -0,0 +1,14 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('add a new survey', {:type => :feature}) do
+  it('allows user to enter text for new survey') do
+    visit('/')
+    click_link('Create a new Survey')
+    fill_in('name', :with => 'New Gallup Poll')
+    click_button('Create Survey')
+    expect(page).to have_content('NEW GALLUP POLL')
+  end
+end
